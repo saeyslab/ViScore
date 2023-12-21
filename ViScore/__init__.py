d2494e90632a327c1ad5e0caf0f123b1d07dc7eb
David Novak <david.novak@ugent.be>
2023-12-21 22:31:21 +0100

    Export palette

--- ViScore/__init__.py
+++ ViScore/__init__.py
@@ -1,3 +1,3 @@
-from .supervised import xnpe, plot_xnpe_map, max_val_in_dict, neighbourhood_composition, neighbourhood_composition_plot
+from .supervised import xnpe, plot_xnpe_map, max_val_in_dict, neighbourhood_composition, neighbourhood_composition_plot, PALETTE
 from .unsupervised import score
 from .aux_knn import make_knn
