c7d6ede81c772c8852b775b0e73b443e7b38a0fd
David Novak <david.novak@ugent.be>
2024-07-03 23:51:39 +0200

    Update venv recipes

--- benchmarking/install/SAUCIE_install.sh
+++ benchmarking/install/SAUCIE_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 pip install --no-input --use-pep517 git+https://github.com/davnovak/SAUCIE.git
+pip install --no-input --upgrade-strategy only-if-needed six typing-extensions packaging requests decorator wheel==0.43
 pip install --no-input --upgrade-strategy only-if-needed numpy six scikit-learn
